5f1e554a04657e46d67aa4ef7a98315771ed6dd3
Matheus Martins <matheus.math27@hotmail.com>
2021-02-27 04:15:47 -0300

    fix cors cross origin

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,18 @@
 const express = require('express');
 const cors = require('cors');
-const { json } = require('express');
 const routes = require('./src/routes');
 
 const app = express();
-app.use(cors());
-app.use(json());
+app.use(
+  cors({
+    methods: ['GET', 'POST', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Access-Control-Allow-Origin', 'Accept'],
+    origin: '*',
+    credentials: true,
+    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+  })
+);
+app.use(express.json());
 app.use(routes);
 
-app.listen(process.env.PORT || 1337, () =>
-  console.log('[app] started!')
-);
+app.listen(process.env.PORT || 1337, () => console.log('[app] started!'));
