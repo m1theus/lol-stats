ee4d1ab61a474709dac30c3b6b9851631541fbd3
Matheus Martins <matheus.math27@hotmail.com>
2021-02-27 02:59:23 -0300

    config to vercel

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const cors = require('cors');
 const { json } = require('express');
-const routes = require('./routes');
+const routes = require('./src/routes');
 
 const app = express();
 app.use(cors());
 app.use(json());
 app.use(routes);
 
-app.listen(process.env.SERVER_PORT || 1337, () =>
+app.listen(process.env.PORT || 1337, () =>
   console.log('[app] started!')
 );
